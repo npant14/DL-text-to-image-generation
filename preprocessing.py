9f70ab232cdb1c9a3bd18684a746c100fc51a4ab
Jay Sarva <sarva.jay.s@gmail.com>
2021-11-30 08:11:30 -0500

    Initial preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,21 @@
+from pycocotools.coco import COCO
+import numpy as np
+
 ## for the preprocessing code
 
 ## Image preprocessing
 
 ## caption preprocessing
+
+def get_data(annFile):
+    """
+    Given annotation file path, return array of images and array of captions corresponding to these images (as strings)
+    """
+    coco = COCO(annFile)
+    anns = coco.loadAnns(coco.getAnnIds())
+    img_ids = map(lambda x: x.image_id, anns)
+    captions = map(lambda x: x.caption, anns)
+    imgs = coco.loadImgs(img_ids)
+
+    return imgs, captions
+
