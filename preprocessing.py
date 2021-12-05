e37d5ea0502b12029eede18fa844f4a9d1a8560b
Jay Sarva <sarva.jay.s@gmail.com>
2021-12-05 14:22:22 -0500

    Update preprocessing with links

--- preprocessing.py
+++ preprocessing.py
@@ -11,7 +11,12 @@ def get_data(annFile):
     """
     Given annotation file path, return array of images and array
     of captions corresponding to these images (as strings)
-    """
+    """ # http://images.cocodataset.org/zips/train2014.zip
+    #   http://images.cocodataset.org/annotations/annotations_trainval2014.zip
+    # For Test:::
+    #### http://images.cocodataset.org/zips/test2015.zip
+    #### http://images.cocodataset.org/annotations/image_info_test2015.zip
+    
     coco = COCO(annFile)
     anns = coco.loadAnns(coco.getAnnIds())
     img_ids = map(lambda x: x.image_id, anns)
