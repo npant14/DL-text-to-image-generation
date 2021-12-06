1dcfc464d6e6ca964ea1fdc98fd31d1956aaaf2a
linus-sun <linus.a.sun@gmail.com>
2021-12-06 01:36:20 -0500

    preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,28 +1,53 @@
-from pycocotools.coco import COCO
 import numpy as np
-
+import tensorflow as tf
+from PIL import Image
 ## for the preprocessing code
 
 ## Image preprocessing
 
 ## caption preprocessing
 
-def get_data(annFile):
+def get_data():
     """
     Given annotation file path, return array of images and array
     of captions corresponding to these images (as strings)
-    """ # http://images.cocodataset.org/zips/train2014.zip
-    #   http://images.cocodataset.org/annotations/annotations_trainval2014.zip
-    # For Test:::
-    #### http://images.cocodataset.org/zips/test2015.zip
-    #### http://images.cocodataset.org/annotations/image_info_test2015.zip
+    """ 
+    folder = "CUB_200_2011"
+    classes_txt = open(folder + "/classes.txt")
+    classes = classes_txt.read()
+    classes_txt.close()
+    classnames = classes.splitlines()
+    classnames = list(map(lambda x: x[x.find(' ') + 1:], classnames))
     
-    coco = COCO(annFile)
-    anns = coco.loadAnns(coco.getAnnIds())
-    img_ids = map(lambda x: x.image_id, anns)
-    captions = map(lambda x: x.caption, anns)
-    imgs = coco.loadImgs(img_ids)
-    imgs_np = map(lambda x: np.resize((64, 64), np.asarray(x)), imgs)
+    img_class_labels_txt = open(folder + "/image_class_labels.txt")
+    img_class_labels = img_class_labels_txt.read()
+    img_class_labels_txt.close()
+    
+    img_class_labels = img_class_labels.splitlines()
+    
+    img_attributes = []
+    attrib_list = open(folder + "/attributes/image_attribute_labels.txt")
+    img_attribs = attrib_list.read()
+    attrib_list.close()
+    img_attribs = img_attribs.splitlines()
+    for i in range(0, 11788):
+        img_attributes.append(list(map(lambda x: int(x[x.find(' ', x.find(' ')+1)+1:x.find(' ', x.find(' ')+1)+2]), img_attribs[i:i+312])))
+    print(tf.convert_to_tensor(img_attributes))
+    
+    images = []
+    image_list = open(folder + "/images.txt")
+    image_ids = image_list.read()
+    image_list.close()
+    image_ids = image_ids.splitlines()
+    for i in range(0, 11788):
+        img_data = Image.open(folder + "/images/" + image_ids[i][image_ids[i].find(' ')+1:], 'r').getdata()
+        images.append(np.array(img_data))
+    print(0)
+    return 0
 
-    return imgs_np, captions
 
+
+def main():
+    get_data()
+    
+main()
